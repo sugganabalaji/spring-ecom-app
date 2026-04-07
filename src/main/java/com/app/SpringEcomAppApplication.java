ca07783494d2f9b57ba1cc0acfe345834bf2029b
Balaji, Suggana <sugganabalaji@gmail.com>
2026-04-08 04:45:20 +0530

    Initial commit

---
+++ src/main/java/com/app/SpringEcomAppApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcomAppApplication.class, args);
+	}
+
+}
