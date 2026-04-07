d17b2efecd7688ed0982f07b5d1c9ca2b07c4c09
Balaji, Suggana <sugganabalaji@gmail.com>
2026-04-08 05:03:52 +0530

    findAllProducts from DB

--- src/main/java/com/app/controller/ProductController.java
+++ src/main/java/com/app/controller/ProductController.java
@@ -1,15 +1,23 @@
 package com.app.controller;
 
+import com.app.controller.repository.service.ProductService;
+import com.app.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
 
+    @Autowired
+    private ProductService service;
+
     @GetMapping("/products")
-    public String getProducts() {
-        return "products";
+    public List<Product> getProducts() {
+        return service.findAll();
     }
 }
---
+++ src/main/java/com/app/controller/repository/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.app.controller.repository.service;
+
+import com.app.controller.repository.ProductRepository;
+import com.app.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+}
